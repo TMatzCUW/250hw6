0b0f3199c0a3b9d21ee024b230e023fdd37dee18
TMatzCUW <70924911+TMatzCUW@users.noreply.github.com>
2020-09-30 13:37:48 -0500

    Books and Sum file

--- src/hw6.java
+++ src/hw6.java
@@ -1,4 +1,21 @@
 
 public class hw6 {
 
+public class Biblebook {
+	
+		public String name;
+		public int chapter;
+		public String desc;
+		
+		public Biblebook(String name, int chapter, String desc){
+			this.name=name;
+			this.chapter=chapter;
+			this.desc=desc;
+		}
+		
+		public void display() {
+			System.out.println(this.name+" has "+this.chapter+" chapters and is "+this.desc+".");
+		}
+	}
+
 }
